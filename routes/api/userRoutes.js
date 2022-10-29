04c44b352e5b627d2e1cfa810a982dfaf22b2f83
tedtalktimmy <frtntn007@gmail.com>
2022-10-29 01:36:35 -0400

    completed uRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,16 @@
 const router = require('express').Router();
 const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('../../controllers/userController');
 
+router
+.route('/')
+.get(getAllUsers)
+.post(createUser);
+
+router
+.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
+
 
 module.exports = router;

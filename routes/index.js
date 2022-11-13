c4336e870793f4963aeb3493c3c0f769f87f0281
tedtalktimmy <frtntn007@gmail.com>
2022-11-13 15:15:21 -0500

    reworking files for functionality

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
+
 const apiRoutes = require('./api');
+
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  return res.status(404).send('wrong route, bruv');
-});
-
+router.use((req, res) => res.send('Wrong route!'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
-const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
 app.use(routes);
 
-
-
-
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API running on port ${PORT}`);
-  });
+//BCS Learning Assistant Helped 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
